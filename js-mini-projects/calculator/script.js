a63108088c3d6a1ce5c0a1bd1e68078e493d8745
guddu kumar <146803696+KrGuddu@users.noreply.github.com>
2025-07-30 12:58:07 +0000

    feat: updated index.html and script.js

--- js-mini-projects/calculator/script.js
+++ js-mini-projects/calculator/script.js
@@ -1,2 +1,19 @@
 // Write your JS here
-console.log("Mini project ready!");
+// console.log("Mini project ready!");
+
+
+function calculate(){
+    const n1 = parseFloat(document.getElementById("num1").value);
+    const n2 = parseFloat(document.getElementById("num2").value);
+    const op = document.getElementById("operator").value;
+    let res = 0;
+           
+    switch(op){
+    case "+" : res= n1+n2; break;
+    case "-" : res=n1-n2; break;
+    case "*" : res=n1*n2; break;
+    case "/" : res= n1/n2; break;
+    }
+
+    document.getElementById("result").innerHTML="Result : "+res;
+}
