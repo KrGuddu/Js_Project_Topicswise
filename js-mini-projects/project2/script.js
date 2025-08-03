f15865667198e4d19cc96314ccdae4b3a74ddb3b
guddu kumar <146803696+KrGuddu@users.noreply.github.com>
2025-08-03 10:31:13 +0000

    Create Votting App

--- js-mini-projects/project2/script.js
+++ js-mini-projects/project2/script.js
@@ -1,2 +1,8 @@
-// Write your JS here
-console.log("Mini project ready!");
+function checkAge(){
+   const age = parseInt(document.getElementById("age").value)
+   if (age >= 18) {
+    document.getElementById("message").innerText= "You are eligible for vote!"
+   } else {
+    document.getElementById("message").innerText= "Sorry, Keep 18+"
+   }
+}
